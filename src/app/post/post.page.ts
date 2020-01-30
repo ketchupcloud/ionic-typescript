4a25df396426900c5a3c0a4d2676dfecc35e0afd
Alunos <juliapohlmann@hotmail.com>
2020-01-30 16:06:43 -0300

    dislikeSolved

--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -10,25 +10,48 @@ export class PostPage implements OnInit {
   constructor() {}
 	Posts= [{
 		username: 'Jubs',
-		like: false,
+		StatusLike: null,
 		quantLikes: 0,
+		quantDislikes: 0,
 		spoiler: true
 	},
 	{
 		username: 'Tef',
-		like: true,
+		statusLike: null,
 		quantLikes: 25,
+		quantDislikes: 4,
 		spoiler: false
 	}];
 	
 	likePost(post:any){
-		if(post.like){
+		if(post.statusLike===true){
 			post.quantLikes--;
-			post.like = false;
+			post.statusLike = null;
 		}
-		else{
+		else if(post.statusLike===false){
 			post.quantLikes++;
-			post.like = true;
+			post.statusLike = true;
+			post.quantDislikes--;
+		}
+		else {
+			post.quantLikes++;
+			post.statusLike = true;
+		}
+	}
+	
+	dislikePost(post:any){
+		if(post.statusLike===true){
+			post.quantDislikes++;
+			post.quantLikes--;
+			post.statusLike = false;
+		}
+		else if(post.statusLike===false){
+			post.statusLike = null;
+			post.quantDislikes--;
+		}
+		else {
+			post.quantDislikes++;
+			post.statusLike = false;
 		}
 	}
 	
